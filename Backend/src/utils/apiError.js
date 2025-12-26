3280cd37517fbe3446c07a9a0d2cfba8791fbc1b
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2025-12-26 04:14:53 +0000

    refactor: change code block file

--- Backend/src/utils/apiError.js
+++ Backend/src/utils/apiError.js
@@ -1,1 +1,15 @@
-//add utility functions
+class ApiError extends Error {
+    constructor(statusCode,
+        message = "something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+    }
+}
+export { ApiError };
