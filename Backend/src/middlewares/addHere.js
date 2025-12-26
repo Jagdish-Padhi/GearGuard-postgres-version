d9683ed257e736ffbd9b5015b5a6c6509981f287
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2025-12-26 04:09:15 +0000

    feat: Add centralized error handling utility class

--- Backend/src/middlewares/addHere.js
+++ Backend/src/middlewares/addHere.js
@@ -1,1 +1,15 @@
-//add any middleware needed
+class ApiError extends Error {
+    constructor(statusCode,
+        message = "something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+    }
+}
+export { ApiError };
