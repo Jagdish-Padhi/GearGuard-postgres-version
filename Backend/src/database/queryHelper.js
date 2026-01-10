3c8198c7c47dc9015679812b2c019bd90b21e1bc
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2026-01-10 17:53:19 +0000

    feat: create query helper functions

---
+++ Backend/src/database/queryHelper.js
@@ -0,0 +1,34 @@
+import { text } from 'express';
+import pool from './connect.js';
+
+// execute query with error handling
+export const query = async (text, params) => {
+
+    try {
+        const result = await pool.query(text, params);
+        return result;
+    } catch (error) {
+        console.error('Database query error: ', error);
+        throw error;
+    }
+};
+
+// get single row
+export const getOne = async (text, params) => {
+    const result = await query (text, params);
+    return result.rows[0] || null;
+};
+
+// get multiple rows
+export  const  getMany = async (text, params)=>{
+     const result = await query(text, params);
+     return result.rows[0] || null;
+};
+
+// Insert and return
+export const insertAndReturn = async (text, params) => {
+    const result = await query(text, params);
+    return result.rows[0];
+};
+
+export default { query, getOne, getMany, insertAndReturn };
